266f58d2c8c9d5c450abaac881a6cb254ee21e89
young-st511 <asdk511@gmail.com>
2022-11-29 15:45:38 +0900

    feat: 라우팅 추가

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,15 +1,29 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
+import Chat from './pages/Chat';
+import Community from './pages/Community';
+import Profile from './pages/Profile';
+import Root from './pages/Root/Root';
 import GlobalStyle from './themes/GlobalStyle.style';
 import mainTheme from './themes/theme';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    children: [
+      { path: 'chat', element: <Chat /> },
+      { path: 'community', element: <Community /> },
+      { path: 'profile', element: <Profile /> },
+    ],
+  },
+]);
 
 function Router() {
   return (
     <ThemeProvider theme={mainTheme}>
       <GlobalStyle />
-      <div className="App">
-        {/*
-         */}
-      </div>
+      <RouterProvider router={router} />
     </ThemeProvider>
   );
 }

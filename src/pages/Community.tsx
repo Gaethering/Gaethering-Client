06b1d56edf96522d820a0409b0c95ebccba401f2
Inae Choi <inaechoi89@gmail.com>
2022-12-11 01:34:47 +0900

    refactor(community): community 페이지 폴더 구조 변경
    #36

---
+++ src/pages/Community.tsx
@@ -0,0 +1,14 @@
+import NavBar from '../components/NavBar';
+import ArticleList from '../components/Community/ArticleList';
+
+function Community() {
+  return (
+    <div>
+      <NavBar />
+      <ArticleList />
+    </div>
+  );
+}
+
+export default Community;
+export { ArticleList };

99797bedff019fa29295c4be166ffc5a74e2a09c
young-st511 <asdk511@gmail.com>
2022-12-14 19:42:12 +0900

    feat(API): API 환경변수 설정

--- src/api/axiosConfig.ts
+++ src/api/axiosConfig.ts
@@ -1,4 +1,6 @@
 import axios from 'axios';
 import { API_BASE_URL } from '../data/API_ENV';
 
-axios.defaults.baseURL = API_BASE_URL;
+export const axiosDefaultsConfig = () => {
+  axios.defaults.baseURL = API_BASE_URL;
+};

766fd710bba7c37dc66fcf71c5ac938cba2d5738
young-st511 <asdk511@gmail.com>
2022-12-15 16:57:17 +0900

    정규표현식 수정

--- src/data/regExp.ts
+++ src/data/regExp.ts
@@ -1,9 +1,8 @@
 export const EMAIL_REGEX = /^\w+[-_.\w]*@+\w+[-_.\w]+\.[a-zA-Z]{2,3}$/;
 
-export const PASSWORD_REGEX = /[-\w._!@#$%^&*?]{8,15}/;
+export const PASSWORD_REGEX =
+  /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,15}$/;
 
 //(?=.*[0-91)(?=.*[a-2])(2=.*11w)(?=115+5){46,153}
-
-export const NICKNAME_REGEX = /\w{2, 10}/;
 
 export const PHONE_REGEX = /^[0-9]{2,6}-[-0-9]{2,10}$/;

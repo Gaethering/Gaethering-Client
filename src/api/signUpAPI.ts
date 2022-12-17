d67a5792085eee14078b731264f442071c18ffc7
young-st511 <asdk511@gmail.com>
2022-12-17 16:53:14 +0900

    LogInError 상세히 보기

--- src/api/signUpAPI.ts
+++ src/api/signUpAPI.ts
@@ -1,16 +1,29 @@
+import axios, { Axios, AxiosError, AxiosResponse } from 'axios';
 import { postRequest } from './requests';
 import { SignUpApiUrl, SignUpResponse } from './signUpAPI.type';
 
 export const postSignUp = async (data: FormData) => {
-  const response = await postRequest<SignUpResponse, FormData>(
-    SignUpApiUrl.SIGN_UP,
-    data,
-    {
+  try {
+    const response = await axios.post<
+      SignUpResponse,
+      AxiosResponse<SignUpResponse, FormData>,
+      FormData
+    >(SignUpApiUrl.SIGN_UP, data, {
       headers: {
         'Content-Type': 'multipart/form-data',
       },
-    }
-  );
+    });
 
-  return response;
+    return response;
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      console.error('name:' + error.name, 'status:' + error.status);
+      console.error('cause:' + error.cause);
+      console.error('code:' + error.code);
+      console.error('response:' + error.response);
+      console.error('message:' + error.message);
+      console.error('JSON:', Object(error.toJSON()));
+      console.error(error);
+    }
+  }
 };

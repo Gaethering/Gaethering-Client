ba426848174dea7daef85bf9cdf2f162d6d9fe0b
young-st511 <asdk511@gmail.com>
2022-12-17 17:38:11 +0900

    수정

--- src/api/showAxiosError.ts
+++ src/api/showAxiosError.ts
@@ -4,10 +4,6 @@ import { AxiosError } from 'axios';
 function showAxiosError(error: AxiosError<any, any>) {
   console.error('REQ:', error.request);
   console.error('code: ' + error.response?.data?.code, error.code);
-<<<<<<< HEAD
-=======
-  console.error('response:', error.response);
->>>>>>> cc4c46c6eaf9fd913a67f6099cdb7c5a88958904
   console.error('message: ', error.response?.data?.message, error.message);
   console.error(error);
 }

4ffd569181564c78282f5e1fb40eaf718cb54eac
young-st511 <asdk511@gmail.com>
2022-12-17 19:19:38 +0900

    feat(Chat): 스켈레톤 MyChatRoom 생성

--- src/components/Chat/MyChatRoom.tsx
+++ src/components/Chat/MyChatRoom.tsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import useRelativeTime from '../../Hooks/useRelativeTime';
 import { ChatRoomInfo } from './Chat.type';
+import { Left, Right, StyledMyChatRoom, Unread } from './MyChatRoom.style';
 
 function MyChatRoom({
   roomName,
@@ -19,7 +20,7 @@ function MyChatRoom({
           <h2>
             {roomName} <span>{participants}</span>
           </h2>
-          <p>{lastChat}</p>
+          <p className="last-chat">{lastChat}</p>
         </Left>
         <Right>
           <p>{useRelativeTime(lastChatTime)}</p>
@@ -31,65 +32,3 @@ function MyChatRoom({
 }
 
 export default MyChatRoom;
-
-const StyledMyChatRoom = styled.div`
-  a {
-    display: flex;
-    justify-content: space-between;
-
-    margin: 3rem 0;
-    padding: 2rem 3rem;
-
-    height: 6rem;
-
-    background-color: ${({ theme: { color } }) => color.white};
-    border-radius: 1.6rem;
-    box-shadow: 0 0 2rem 0 rgba(0, 0, 0, 0.1);
-
-    text-decoration: none;
-    font-style: normal;
-  }
-`;
-
-const Left = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  justify-content: space-between;
-
-  h2 {
-    display: flex;
-    align-items: center;
-    font-size: 2.4rem;
-    span {
-      margin-left: 1rem;
-
-      color: ${({ theme: { color } }) => color.main};
-      font-size: 2rem;
-      font-weight: 600;
-    }
-  }
-`;
-
-const Right = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-end;
-  justify-content: space-between;
-`;
-
-const Unread = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-
-  width: 2.6rem;
-  height: 2.6rem;
-
-  font-size: 1.4rem;
-  font-weight: 600;
-  color: ${({ theme: { color } }) => color.white};
-
-  border-radius: 100%;
-  background-color: ${({ theme: { color } }) => color.redPoint};
-`;
--- src/components/Chat/MyChatRoomList.tsx
+++ src/components/Chat/MyChatRoomList.tsx
@@ -2,23 +2,38 @@ import { useQuery } from 'react-query';
 import { getRequest } from '../../api/requests';
 import { ChatRoomInfo } from './Chat.type';
 import MyChatRoom from './MyChatRoom';
+import MyChatRoomSkeleton from './MyChatRoomSkeleton';
 
 // type Props = {}
 
 function MyChatRoomList() {
-  const { data: query } = useQuery('chatrooms', () =>
+  const { data: query, isLoading } = useQuery('chatrooms', () =>
     getRequest<ChatRoomInfo[]>('chatrooms')
   );
   const chatRooms = query?.data;
 
   return (
-    <div>
-      {chatRooms
-        ?.sort((x, y) => parseInt(y.lastChatTime) - parseInt(x.lastChatTime))
-        .map((elem) => (
-          <MyChatRoom {...elem} key={elem.roomKey} />
-        ))}
-    </div>
+    <>
+      {isLoading ? (
+        <>
+          <MyChatRoomSkeleton />
+          <MyChatRoomSkeleton />
+          <MyChatRoomSkeleton />
+        </>
+      ) : (
+        <>
+          <div>
+            {chatRooms
+              ?.sort(
+                (x, y) => parseInt(y.lastChatTime) - parseInt(x.lastChatTime)
+              )
+              .map((elem) => (
+                <MyChatRoom {...elem} key={elem.roomKey} />
+              ))}
+          </div>
+        </>
+      )}
+    </>
   );
 }
 

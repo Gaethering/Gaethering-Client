450f8174670092608366e54aab5401f409eb26e5
young-st511 <asdk511@gmail.com>
2022-12-20 21:47:50 +0900

    fix(Nav): NavServiceName Context 추가

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -1,12 +1,19 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import ChatNav from '../components/Chat/ChatNav';
 import FindChatSection from '../components/Chat/FindChatSection';
 import MyChatSection from '../components/Chat/MyChatSection';
+import { useSetServiceName } from './Root';
 
 export type ChatState = 'findChat' | 'myChat';
 
 function Chat() {
   const [chatState, setChatState] = useState<ChatState>('findChat');
+  const setNav = useSetServiceName();
+
+  useEffect(() => {
+    setNav('개모임');
+  }, [setNav]);
+
   return (
     <div>
       <ChatNav chatState={chatState} setChatState={setChatState} />

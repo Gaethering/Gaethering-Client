e4433b982d8c4d580a5ba4ea106460beffe3c460
young-st511 <asdk511@gmail.com>
2022-12-22 02:29:49 +0900

    Mock API 로그 제거

--- src/mocks/handler.ts
+++ src/mocks/handler.ts
@@ -1,24 +1,9 @@
 import { rest } from 'msw';
-import { AuthApiUrl } from '../api/authAPI.type';
-import { SignUpApiUrl } from '../api/signUpAPI.type';
-import { SignUpForm } from '../components/SignUp/SignUp.type';
-
 import { mockChatRooms } from './mockChatRooms';
 import { mockChatTalk } from './mockChatTalk';
 
-interface LoginBody {
-  userID: string;
-}
-interface LoginResponse {
-  username: string;
-  age: string;
-}
-
-const auth = {};
-
 export const handlers = [
   rest.get('/chatrooms', (req, res, ctx) => {
-    console.log(mockChatRooms);
     return new Promise((resolve) =>
       setTimeout(
         () => resolve(res(ctx.status(200), ctx.json(mockChatRooms))),
@@ -27,7 +12,6 @@ export const handlers = [
     );
   }),
   rest.get('/chatroom/efadc2/info', (req, res, ctx) => {
-    console.log(mockChatRooms[0]);
     return new Promise((resolve) =>
       setTimeout(
         () => resolve(res(ctx.status(200), ctx.json(mockChatRooms[0]))),
@@ -36,7 +20,6 @@ export const handlers = [
     );
   }),
   rest.get('/chatroom/efadc2/talks', (req, res, ctx) => {
-    console.log(mockChatTalk);
     return new Promise((resolve) =>
       setTimeout(
         () => resolve(res(ctx.status(200), ctx.json(mockChatTalk))),

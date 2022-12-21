e5bf3dd8bab3d3066136c8e6df75a0a12e4fcb65
young-st511 <asdk511@gmail.com>
2022-12-22 00:49:16 +0900

    feat(Nav): Nav 선택시 NavSelector 자동 숨기기

--- src/components/NavBar/NavSelector.tsx
+++ src/components/NavBar/NavSelector.tsx
@@ -1,6 +1,6 @@
 import React, { Dispatch } from 'react';
 import { NavLink } from 'react-router-dom';
-import { NavURL, SetServiceType } from './NavBar.type';
+import { NavURL, ServiceType, SetServiceType } from './NavBar.type';
 import { StyledModalOverlay, StyledNavSelector } from './NavSelector.style';
 
 interface Props {
@@ -9,28 +9,33 @@ interface Props {
   setShowNav: Dispatch<React.SetStateAction<boolean>>;
 }
 
-function NavSelector({ setServiceName: set, showNav, setShowNav }: Props) {
+function NavSelector({ setServiceName, showNav, setShowNav }: Props) {
+  const setNav = (service: ServiceType) => {
+    setServiceName(service);
+    setShowNav(false);
+  };
+
   return (
     <>
       <StyledNavSelector className={showNav ? 'view-nav' : 'hidden'}>
         <ul>
           <li>
-            <NavLink to={NavURL.개모임} onClick={() => set('개모임')}>
+            <NavLink to={NavURL.개모임} onClick={() => setNav('개모임')}>
               개모임
             </NavLink>
           </li>
           <li>
-            <NavLink to={NavURL.동네소식} onClick={() => set('동네소식')}>
+            <NavLink to={NavURL.동네소식} onClick={() => setNav('동네소식')}>
               동네소식
             </NavLink>
           </li>
           <li>
-            <NavLink to={NavURL.멍그램} onClick={() => set('멍그램')}>
+            <NavLink to={NavURL.멍그램} onClick={() => setNav('멍그램')}>
               멍그램
             </NavLink>
           </li>
           <li>
-            <NavLink to={NavURL.프로필} onClick={() => set('프로필')}>
+            <NavLink to={NavURL.프로필} onClick={() => setNav('프로필')}>
               프로필
             </NavLink>
           </li>
@@ -38,7 +43,7 @@ function NavSelector({ setServiceName: set, showNav, setShowNav }: Props) {
       </StyledNavSelector>
       <StyledModalOverlay
         className={showNav ? 'view-nav' : 'hidden'}
-        onClick={() => setShowNav((prev) => !prev)}
+        onClick={() => setShowNav(false)}
       />
     </>
   );

9d7ad7e0f70f99201594144130d5c42eb05d3f00
Inae Choi <inaechoi89@gmail.com>
2022-12-25 18:35:44 +0900

    fix(profile): selectInput 오류 수정

---
+++ src/components/Profile/Edit.type.ts
@@ -0,0 +1,11 @@
+export interface EditPetForm {
+  name: string;
+  birth: string;
+  gender: string;
+  breed: string;
+  weight: number;
+  isNeutered: boolean;
+  description: string;
+  imageUrl: string;
+}
+

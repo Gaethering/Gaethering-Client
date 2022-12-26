8067cb35ae76f8441b006ee1b6c76da455e26f0c
young-st511 <asdk511@gmail.com>
2022-12-27 05:58:30 +0900

    feat(Commu): API 호출 함수 작성

--- src/api/boardAPI.ts
+++ src/api/boardAPI.ts
@@ -1,13 +1,15 @@
 import axios, { AxiosResponse } from 'axios';
 import { API_BASE_URL } from '../data/API_ENV';
 import * as T from './boardAPI.type';
+import { BoardApiUrl as Api } from './boardAPI.type';
 
-type Res = T.PostArticleResponse;
 type A<T, D> = AxiosResponse<T, D>;
+type PostId = number;
 
 export async function postQnaArticle(data: FormData) {
+  type Res = T.PostArticleResponse;
   const response = await axios.post<Res, A<Res, FormData>, FormData>(
-    T.BoardApiUrl.PostArticle + '1',
+    Api.PostArticle + '1',
     data,
     {
       headers: {
@@ -19,8 +21,9 @@ export async function postQnaArticle(data: FormData) {
   return response.data;
 }
 export async function postInfoArticle(data: FormData) {
+  type Res = T.PostArticleResponse;
   const response = await axios.post<Res, A<Res, FormData>, FormData>(
-    T.BoardApiUrl.PostArticle + '2',
+    Api.PostArticle + '2',
     data,
     {
       headers: {
@@ -29,5 +32,63 @@ export async function postInfoArticle(data: FormData) {
       baseURL: API_BASE_URL,
     }
   );
+  return response.data;
+}
+
+async function getArticles(
+  size = 10,
+  categoryId: T.CategoryID,
+  lastId?: number
+) {
+  type Res = T.GetArticlesResponse;
+  const response = await axios.get<Res, A<Res, void>>(
+    `${Api.GetArticles}${categoryId}/list?size=${size}${
+      lastId ? '&lastPostId=' + lastId : ''
+    }`
+  );
+
+  return response.data;
+}
+
+export async function getQnaArticles(size?: number, lastId?: PostId) {
+  return await getArticles(size, T.CategoryID.qna, lastId);
+}
+
+export async function getInfoArticles(size?: number, lastId?: PostId) {
+  return await getArticles(size, T.CategoryID.info, lastId);
+}
+
+export async function patchArticle(id: PostId, data: T.PatchArticleRequest) {
+  type Res = T.PatchArticleResponse;
+  type Req = T.PatchArticleRequest;
+  const response = await axios.patch<Res, A<Res, Req>, Req>(
+    Api.PatchArticle + id,
+    data
+  );
+
+  return response.data;
+}
+
+export async function deleteArticle(id: PostId) {
+  const response = await axios.delete<void>(`${Api.DeleteArticle}${id}`);
+
+  return response.data;
+}
+
+export async function postImage(id: PostId, data: FormData) {
+  type Res = T.PostImageResponse;
+  const response = await axios.post<Res, A<Res, FormData>, FormData>(
+    `${Api.PostImage}${id}/images`,
+    data
+  );
+
+  return response.data;
+}
+
+export async function deleteImage(id: PostId, imageId: number) {
+  const response = await axios.delete<void>(
+    `${Api.DeleteImage}${id}/images/${imageId}`
+  );
+
   return response.data;
 }

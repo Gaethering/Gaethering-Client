155aa7ef8fb78eee7bb45ae52cd8c41c1d35a642
young-st511 <asdk511@gmail.com>
2022-12-29 15:54:13 +0900

    API Base URL 변경

--- src/data/API_ENV.ts
+++ src/data/API_ENV.ts
@@ -1,2 +1,2 @@
 // export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
-export const API_BASE_URL = 'http://localhost:8080/api';
+export const API_BASE_URL = 'http://3.34.98.103:8080/api';

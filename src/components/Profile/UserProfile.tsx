b4e86d56d91026e7ccb402c46718921c1a055799
Inae Choi <inaechoi89@gmail.com>
2022-12-29 16:19:32 +0900

    fix(profile): 내 게시물 조회 삭제

--- src/components/Profile/UserProfile.tsx
+++ src/components/Profile/UserProfile.tsx
@@ -1,11 +1,10 @@
 import { StyledUserProfile } from './User.style';
 import Button from '../Form/Button';
 import PetImage from './PetImage';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { useMutation } from 'react-query';
 import { patchProfile } from '../../api/profileAPI';
-import { SubmitHandler, useForm } from 'react-hook-form';
 
 interface UserProp {
   userName: string;
@@ -43,12 +42,6 @@ function UserProfile({ userName, petImg }: UserProp) {
         </div>
       </div>
       <div className="button_container">
-        <Link to="myBoard" className="link">
-          <Button btnTheme="sub" type="button" className="go_my_article">
-            내가 쓴 게시물
-          </Button>
-        </Link>
-
         <Link to="edit" className="link">
           <Button
             btnTheme="main"
